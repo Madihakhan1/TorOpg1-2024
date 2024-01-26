a1f37131a24e13da842954c0c7c297128ced5870
Madihakhan1 <144325902+Madihakhan1@users.noreply.github.com>
2024-01-26 15:10:12 +0100

    add

---
+++ Main.java
@@ -0,0 +1,21 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        //2.a store your name in a variable and print it.
+        String name = "Madiha";
+        System.out.println(name);
+
+       //2.b store your age in a varible and print it.
+        int age = 25;
+        System.out.println(age);
+
+        //2.c store whether or not you are happy right now, as a boolean (true for happy, false for sad).
+
+        boolean happy = true;
+        boolean isSad = false;
+        System.out.println(isSad);
+
+        System.out.println("Hi my name is "  + name + " \nI am" + age + " years old" + "\ni dont clap my hands");
+    }
+}
